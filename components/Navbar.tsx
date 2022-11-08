ee8719c113dc4e3dd71bcb56e887d96dc06411e3
Luc Perkins <lucperkins@gmail.com>
2022-11-08 11:01:41 -0300

    Add very ugly dropdown for system

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,14 @@
+import { Menu } from "@headlessui/react";
 import clsx from "clsx";
 import { state, systems } from "lib/state";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import site from "site";
 import shallow from "zustand/shallow";
+
+type State = {
+  active: boolean;
+};
 
 const Navbar = () => {
   const [system, setSystem] = state((s) => [s.system, s.setSystem], shallow);
@@ -24,16 +29,33 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div className="flex space-x-2">
-          {systems.map((s) => (
-            <button
-              key={s}
-              className={clsx(s === sys ? "font-bold" : "font-normal")}
-              onClick={(_e) => setSystem(s)}
+        <div>
+          <Menu as="div" className="relative inline-block">
+            <Menu.Button
+              as="button"
+              className="w-24 bg-black text-white py-2 px-3 rounded-lg font-semibold tracking-tight"
             >
-              {s}
-            </button>
-          ))}
+              {sys}
+            </Menu.Button>
+            <Menu.Items className="absolute bg-slate-100 right-0 w-36 mt-2 text-right p-4 shadow">
+              {systems
+                .filter((s) => s != sys)
+                .map((s) => (
+                  <Menu.Item key={s} as={Fragment}>
+                    {({ active }: State) => (
+                      <button
+                        onClick={() => setSystem(s)}
+                        className={clsx(
+                          active ? "text-blue-500" : "text-black"
+                        )}
+                      >
+                        {s}
+                      </button>
+                    )}
+                  </Menu.Item>
+                ))}
+            </Menu.Items>
+          </Menu>
         </div>
       </div>
     </nav>
