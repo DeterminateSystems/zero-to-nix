b15a2dc3b9d95d0fd7fdb74ba5984f29e4f7eff2
Luc Perkins <lucperkins@gmail.com>
2022-11-09 15:27:35 -0300

    Move mdx dir under components

--- components/docs/MDX.tsx
+++ components/docs/MDX.tsx
@@ -1,11 +1,11 @@
 import { useMDXComponent } from "next-contentlayer/hooks";
 
-import Concept from "../../mdx/Concept";
-import Harmonic from "../../mdx/Harmonic";
-import Language from "../../mdx/Language";
-import Languages from "../../mdx/Languages";
-import Script from "../../mdx/Script";
-import Topic from "../../mdx/Topic";
+import Concept from "../mdx/Concept";
+import Harmonic from "../mdx/Harmonic";
+import Language from "../mdx/Language";
+import Languages from "../mdx/Languages";
+import Script from "../mdx/Script";
+import Topic from "../mdx/Topic";
 
 type Props = {
   code: string;
