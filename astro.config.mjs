bc2e152cd8e8d873b534435fd65e8aef8637caa5
Luc Perkins <lucperkins@gmail.com>
2022-11-14 13:55:09 -0300

    Remove compress plugin for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,11 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import compress from "astro-compress";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://zero-to-nix.dev",
   integrations: [
-    compress({
-      css: true,
-      html: true,
-      img: true,
-      js: true,
-      svg: true,
-    }),
     mdx(),
     react(),
     sitemap({
