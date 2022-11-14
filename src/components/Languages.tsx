d4961be524d9a78a45b7f4f20b77295146fc1e9a
Luc Perkins <lucperkins@gmail.com>
2022-11-14 12:27:16 -0300

    Use Tab headless component for language switcher

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,5 +1,7 @@
+import { Tab } from "@headlessui/react";
 import { useStore } from "@nanostores/react";
 import clsx from "clsx";
+import { Fragment } from "react";
 
 import { language, languages } from "../data/store";
 
@@ -7,24 +9,30 @@ const Languages = () => {
   const $language = useStore(language);
 
   return (
-    <div>
-      <div className="flex space-x-2">
-        {languages.map((l) => (
-          <button
-            key={l}
-            onClick={() => language.set(l)}
-            className={clsx(
-              "py-1 px-3 rounded-lg shadow font-semibold",
-              l === $language
-                ? "bg-black text-white"
-                : "bg-slate-200 text-black hover:bg-slate-300"
-            )}
-          >
-            {l}
-          </button>
-        ))}
-      </div>
-    </div>
+    <>
+      <Tab.Group
+        defaultIndex={languages.indexOf($language)}
+        onChange={(idx: number) => language.set(languages[idx]!)}
+      >
+        <Tab.List as="div" className="space-x-2">
+          {languages.map((l) => (
+            <Tab as={Fragment}>
+              <button
+                key={l}
+                className={clsx(
+                  "py-1 px-3 rounded-lg shadow font-semibold",
+                  $language === l
+                    ? "bg-black text-white"
+                    : "bg-slate-200 text-black hover:bg-slate-300"
+                )}
+              >
+                {l}
+              </button>
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
+    </>
   );
 };
 
