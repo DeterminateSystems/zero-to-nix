d657a62c9fa0f3e521731135749161e1c320d86b
Luc Perkins <lucperkins@gmail.com>
2022-11-14 14:06:50 -0300

    Fix OS inference function to allow for non-client

--- src/data/store.ts
+++ src/data/store.ts
@@ -1,9 +1,16 @@
 import { persistentAtom } from "@nanostores/persistent";
 
+const defaultSystem: string =
+  typeof window !== "undefined"
+    ? window.navigator.userAgent.indexOf("Mac") != -1
+      ? "macOS"
+      : "Linux"
+    : "Linux";
+
 export const systems: string[] = ["Linux", "macOS"];
 
 export const languages: string[] = ["Python", "JavaScript", "Rust"];
 
 export const language = persistentAtom<string>("language", "Python");
 
-export const system = persistentAtom<string>("system", "Linux");
+export const system = persistentAtom<string>("system", defaultSystem);
