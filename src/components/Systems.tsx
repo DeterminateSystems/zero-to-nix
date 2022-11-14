9f69a3522b4e5f2b224ccff37ac4e4184df60297
Luc Perkins <lucperkins@gmail.com>
2022-11-14 16:18:15 -0300

    Make the selectors a bit nicer

--- src/components/Systems.tsx
+++ src/components/Systems.tsx
@@ -1,6 +1,8 @@
 import { system, systems } from "../data/store";
 import Selector from "./generic/Selector";
 
-const Systems = () => <Selector options={systems} atom={system} />;
+const Systems = () => (
+  <Selector title="Select your platform" options={systems} atom={system} />
+);
 
 export default Systems;
--- src/components/generic/Selector.tsx
+++ src/components/generic/Selector.tsx
@@ -5,34 +5,38 @@ import type { WritableAtom } from "nanostores";
 import { Fragment } from "react";
 
 type Props = {
+  title: string;
   options: string[];
   atom: WritableAtom<string>;
 };
 
-const Selector = ({ options, atom }: Props) => {
+const Selector = ({ title, options, atom }: Props) => {
   const $item: string = useStore(atom);
 
   return (
-    <Tab.Group onChange={(idx: number) => atom.set(options[idx]!)}>
-      <Tab.List as="div" className="space-x-2">
-        {options.map((o) => (
-          <Tab as={Fragment}>
-            <button
-              key={o}
-              onClick={() => atom.set(o)}
-              className={clsx(
-                "py-1 px-3 rounded-lg shadow font-semibold",
-                $item === o
-                  ? "bg-black text-white"
-                  : "bg-slate-200 text-black hover:bg-slate-300"
-              )}
-            >
-              {o}
-            </button>
-          </Tab>
-        ))}
-      </Tab.List>
-    </Tab.Group>
+    <div className="not-prose inline-flex items-center space-x-4 border-2 rounded-lg p-4">
+      <p className="text-xl font-semibold">{title}</p>
+      <Tab.Group onChange={(idx: number) => atom.set(options[idx]!)}>
+        <Tab.List as="div" className="space-x-2">
+          {options.map((o) => (
+            <Tab as={Fragment}>
+              <button
+                key={o}
+                onClick={() => atom.set(o)}
+                className={clsx(
+                  "py-1 px-3 rounded-lg shadow font-semibold",
+                  $item === o
+                    ? "bg-black text-white"
+                    : "bg-slate-200 text-black hover:bg-slate-300"
+                )}
+              >
+                {o}
+              </button>
+            </Tab>
+          ))}
+        </Tab.List>
+      </Tab.Group>
+    </div>
   );
 };
 
