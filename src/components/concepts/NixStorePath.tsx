a014fbca9d6690fcd941c4f474acb38b03c3f83a
Luc Perkins <lucperkins@gmail.com>
2022-11-17 14:54:55 -0300

    Use new color system

--- src/components/concepts/NixStorePath.tsx
+++ src/components/concepts/NixStorePath.tsx
@@ -4,11 +4,11 @@ type Props = {
 
 const NixStorePath = ({ pkg }: Props) => {
   return (
-    <div className="not-prose rounded-lg bg-slate-50 dark:bg-black border-2 border-slate-300 dark:border-slate-500 py-3 px-5">
+    <div className="not-prose rounded-lg bg-pale dark:bg-black border-2 border-light-gray dark:border-gray py-3 px-5">
       <div className="space-y-2 md:space-y-3 lg:space-y-4">
         <pre className="overflow-auto text-lg md:text-xl lg:text-2xl xl:text-3xl flex">
           <div className="flex flex-col md:space-y-1 lg:space-y-1.5">
-            <span className="text-purple-500 dark:text-sky-500 font-mono">
+            <span className="text-lilac dark:text-cerulean font-mono">
               /nix/store/
             </span>
             <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
@@ -16,7 +16,7 @@ const NixStorePath = ({ pkg }: Props) => {
             </span>
           </div>
           <div className="flex flex-col md:space-y-1.5 lg:space-y-1.5">
-            <span className="text-zinc-500 dark:text-slate-400 font-light font-mono">
+            <span className="text-blue dark:text-lilac font-light font-mono">
               sglc12hc6pc68w5ppn2k56n6jcpaci16
             </span>
             <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
@@ -25,9 +25,7 @@ const NixStorePath = ({ pkg }: Props) => {
           </div>
           <span>-</span>
           <div className="flex flex-col md:space-y-1.5 lg:space-y-1.5">
-            <span className="text-orange-500 dark:text-rose-500 font-mono">
-              {pkg}
-            </span>
+            <span className="text-orange dark:text-rose font-mono">{pkg}</span>
             <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
               <strong>3.</strong> Package name
             </span>
