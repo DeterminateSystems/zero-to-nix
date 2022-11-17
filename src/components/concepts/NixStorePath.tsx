e20f7128189998152e4539e6f85d92c36ade6883
Luc Perkins <lucperkins@gmail.com>
2022-11-16 21:28:02 -0300

    Second iteration of component and remove unused

--- src/components/concepts/NixStorePath.tsx
+++ src/components/concepts/NixStorePath.tsx
@@ -1,37 +1,35 @@
-import { createHash } from "crypto";
-
 type Props = {
   pkg: string;
 };
 
 const NixStorePath = ({ pkg }: Props) => {
   return (
-    <div className="not-prose rounded-lg border-2 border-slate-300 dark:border-slate-500 py-3 px-5">
+    <div className="not-prose rounded-lg bg-slate-50 dark:bg-black border-2 border-slate-300 dark:border-slate-500 py-3 px-5">
       <div className="space-y-2 md:space-y-3 lg:space-y-4">
         <pre className="overflow-auto text-lg md:text-xl lg:text-2xl xl:text-3xl flex">
-          <div className="flex flex-col space-y-1.5">
-            <span className="text-blue-700 dark:text-sky-500 font-semibold font-mono">
+          <div className="flex flex-col md:space-y-1 lg:space-y-1.5">
+            <span className="text-purple-500 dark:text-sky-500 font-mono">
               /nix/store/
             </span>
-            <span className="font-sans text-sm md:text-normal lg:text-lg">
-              Root path
+            <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
+              <strong>1.</strong> Root path
             </span>
           </div>
-          <div className="flex flex-col space-y-1.5">
-            <span className="text-slate-400 dark:text-slate-400 font-light font-mono">
+          <div className="flex flex-col md:space-y-1.5 lg:space-y-1.5">
+            <span className="text-zinc-500 dark:text-slate-400 font-light font-mono">
               sglc12hc6pc68w5ppn2k56n6jcpaci16
             </span>
-            <span className="font-sans text-sm md:text-normal lg:text-lg">
-              Input hash
+            <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
+              <strong>2.</strong> Input hash
             </span>
           </div>
           <span>-</span>
-          <div className="flex flex-col space-y-1.5">
-            <span className="text-rose-700 dark:text-rose-500 font-mono">
+          <div className="flex flex-col md:space-y-1.5 lg:space-y-1.5">
+            <span className="text-orange-500 dark:text-rose-500 font-mono">
               {pkg}
             </span>
-            <span className="font-sans text-sm md:text-normal lg:text-lg">
-              Package name
+            <span className="font-sans tracking-tight text-sm md:text-normal lg:text-lg">
+              <strong>3.</strong> Package name
             </span>
           </div>
         </pre>
