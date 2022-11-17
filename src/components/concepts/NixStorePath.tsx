d4e317461802276949a3d4590fe3cea61be52398
Luc Perkins <lucperkins@gmail.com>
2022-11-16 21:18:19 -0300

    Second iteration of Nix store path element

--- src/components/concepts/NixStorePath.tsx
+++ src/components/concepts/NixStorePath.tsx
@@ -1,3 +1,5 @@
+import { createHash } from "crypto";
+
 type Props = {
   pkg: string;
 };
@@ -6,30 +8,33 @@ const NixStorePath = ({ pkg }: Props) => {
   return (
     <div className="not-prose rounded-lg border-2 border-slate-300 dark:border-slate-500 py-3 px-5">
       <div className="space-y-2 md:space-y-3 lg:space-y-4">
-        <pre className="font-mono overflow-auto text-normal md:text-lg lg:text-xl xl:text-2xl">
-          <span className="text-blue-700 dark:text-sky-500 font-semibold">
-            /nix/store/
-          </span>
-          <span className="text-slate-400 dark:text-slate-400 font-light">
-            sglc12hc6pc68w5ppn2k56n6jcpaci16
-          </span>
-          <span>-</span>
-          <span className="text-rose-700 dark:text-rose-500">{pkg}</span>
-        </pre>
-
-        <div className="text-right">
-          <div className="space-x-4">
-            <span className="text-blue-700 dark:text-sky-500">
-              Default Nix store root
+        <pre className="overflow-auto text-lg md:text-xl lg:text-2xl xl:text-3xl flex">
+          <div className="flex flex-col space-y-1.5">
+            <span className="text-blue-700 dark:text-sky-500 font-semibold font-mono">
+              /nix/store/
             </span>
-            <span className="text-slate-400 dark:text-slate-400">
-              Hash of the package's inputs
-            </span>
-            <span className="text-rose-700 dark:text-rose-500">
-              User-supplied name of the package
+            <span className="font-sans text-sm md:text-normal lg:text-lg">
+              Root path
             </span>
           </div>
-        </div>
+          <div className="flex flex-col space-y-1.5">
+            <span className="text-slate-400 dark:text-slate-400 font-light font-mono">
+              sglc12hc6pc68w5ppn2k56n6jcpaci16
+            </span>
+            <span className="font-sans text-sm md:text-normal lg:text-lg">
+              Input hash
+            </span>
+          </div>
+          <span>-</span>
+          <div className="flex flex-col space-y-1.5">
+            <span className="text-rose-700 dark:text-rose-500 font-mono">
+              {pkg}
+            </span>
+            <span className="font-sans text-sm md:text-normal lg:text-lg">
+              Package name
+            </span>
+          </div>
+        </pre>
       </div>
     </div>
   );
