a76637bd1edcd4b37254abe27dbf3702f98573f6
Luc Perkins <lucperkins@gmail.com>
2022-11-20 23:34:12 -0300

    Add Harmonic component

--- src/components/Harmonic.vue
+++ src/components/Harmonic.vue
@@ -1,1 +1,17 @@
-<template></template>
+<script setup lang="ts">
+import { system } from "~/logic/state";
+
+const props = defineProps<{
+  command: string;
+}>();
+
+const { command } = props;
+</script>
+
+<template>
+  <div class="language-shell" data-lang="shell">
+    <pre
+      class="language-shell"
+    ><code>harmonic {{ command }} {{ `${system === "Linux" ? "linux" : "darwin"}-multi` }}</code></pre>
+  </div>
+</template>
