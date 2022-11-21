846be2d02c526d2aabab6dec651e5a8631176781
Luc Perkins <lucperkins@gmail.com>
2022-11-21 15:59:18 -0300

    Remove theme change transition

--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="h-screen bg-white">
-    <div
-      class="transition-theme flex min-h-full flex-col duration-1000 dark:bg-black dark:text-white"
-    >
+    <div class="flex min-h-full flex-col dark:bg-black dark:text-white">
       <slot />
     </div>
   </div>
