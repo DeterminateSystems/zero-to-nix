abfe17959a4171269823b4f0ca18d8f877ba1c4d
Luc Perkins <lucperkins@gmail.com>
2022-11-21 03:07:05 -0300

    Add separate template for about page

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,26 +1,8 @@
-<script setup lang="ts">
-import { usePage } from "iles";
-
-const {
-  frontmatter: { title },
-} = usePage<{
-  title: string;
-}>();
-</script>
-
 <template layout="base">
   <Page>
     <Top>
       <Navbar />
-      <HorizontalContainer>
-        <Hero :title="title" />
-
-        <Panel>
-          <Content>
-            <slot />
-          </Content>
-        </Panel>
-      </HorizontalContainer>
+      <slot />
     </Top>
 
     <Footer />
