edede8756a27707e50e99dbc4113b6d7e522ae08
Luc Perkins <lucperkins@gmail.com>
2022-11-22 17:08:11 -0300

    Add success, info, warning, and danger blocks

---
+++ src/components/Danger.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const { title: dangerTitle } = defineProps<{
+  title?: string;
+}>();
+</script>
+
+<template>
+  <div
+    class="not-prose rounded-lg border-2 border-red bg-pale-red py-4 px-6 dark:bg-black"
+  >
+    <div class="space-y-2">
+      <p v-if="dangerTitle" class="text-2xl font-semibold tracking-tight">
+        {{ dangerTitle }}
+      </p>
+
+      <slot />
+    </div>
+  </div>
+</template>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,13 +15,19 @@ module.exports = {
         // Drawn from: https://tailwindcss.com/docs/customizing-colors
         pale: colors.slate["50"], // #F8FAFC
         yellow: colors.yellow["500"], // #EAB308
+        "pale-yellow": colors.yellow["50"], // #FEFCE8
         "dark-yellow": colors.yellow["600"], // #CA8A04
         "soft-gray": colors.slate["200"], // #E2E8F0
         "light-gray": colors.slate["300"], // #CBD5E1
         gray: colors.slate["500"], // #64748B
         "dark-gray": colors.slate["700"], // #334155
-        "light-blue": colors.blue["300"], // #93C5FD
         blue: colors.blue["500"], // #3B82F6
+        "light-blue": colors.blue["300"], // #93C5FD
+        "pale-blue": colors.blue["50"], // #EFF6FF
+        green: colors.green["500"], // #22C55E
+        "pale-green": colors.green["50"], // #F0FDF4
+        red: colors.red["500"], // #EF4444
+        "pale-red": colors.red["50"], // #FEF2F2
         cerulean: colors.sky["500"], // #0EA5E9
         lilac: colors.purple["500"], // #A855F7
         rose: colors.rose["500"], // #F43F5E
