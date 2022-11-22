4712f1d607061277be3326432c8eac2205904d4a
Luc Perkins <lucperkins@gmail.com>
2022-11-22 01:17:23 -0300

    Add meta-docs to README

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps<{
-  breadcrumb?: {
+  breadcrumb: {
     back: {
       title: string;
       href: string;
@@ -11,16 +11,11 @@ defineProps<{
 </script>
 
 <template>
-  <div
-    v-if="breadcrumb"
-    class="text-normal flex items-center space-x-2.5 text-dark-gray dark:text-white md:text-lg lg:text-xl"
-  >
-    <span>
-      <a :href="breadcrumb.back.href" class="hover:text-primary">{{
-        breadcrumb.back.title
-      }}</a>
-    </span>
-    <span class="font-bold">/</span>
-    <span class="underline decoration-dotted">{{ breadcrumb.title }}</span>
-  </div>
+  <span>
+    <a :href="breadcrumb.back.href" class="hover:text-primary">{{
+      breadcrumb.back.title
+    }}</a>
+  </span>
+  <span class="font-bold">/</span>
+  <span class="underline decoration-dotted">{{ breadcrumb.title }}</span>
 </template>
