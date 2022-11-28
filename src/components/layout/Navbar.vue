c46d8f6427364621eadb26a0f79221ba5628ec41
Luc Perkins <lucperkins@gmail.com>
2022-11-28 00:28:13 -0300

    Add dropdowns on navbar

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -18,17 +18,85 @@
         </div>
 
         <ul class="flex items-center space-x-4 md:space-x-5 lg:space-x-6">
-          <li v-for="(link, idx) in $site.navbarLinks" :key="idx">
-            <a
-              :href="link.href"
-              class="text-normal hover:text-blue dark:hover:text-light-blue md:text-lg lg:text-xl"
-              :class="{
-                'font-semibold': path.startsWith(link.href),
-                'font-normal': !path.startsWith(link.href),
-              }"
-            >
-              {{ link.text }}
-            </a>
+          <li>
+            <Menu as="div" class="relative">
+              <MenuButton
+                class="text-lg hover:text-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                Quick start
+              </MenuButton>
+
+              <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0"
+              >
+                <MenuItems
+                  class="absolute right-0 mt-1.5 w-72 origin-top-right rounded bg-white py-2 shadow ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-black"
+                >
+                  <MenuItem v-slot="{ active }">
+                    <ul class="flex flex-col space-y-1.5">
+                      <li
+                        v-for="({ title, href }, idx) in sortedQuickStartPages"
+                        :key="idx"
+                        class="py-1.5 px-3 hover:bg-pale dark:hover:bg-dark-gray"
+                      >
+                        <a
+                          class="text-sm tracking-tight hover:text-primary md:text-base lg:text-lg"
+                          :class="{ 'bg-blue-500': active }"
+                          :href="href"
+                        >
+                          {{ title }}
+                        </a>
+                      </li>
+                    </ul>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
+          </li>
+          <li>
+            <Menu as="div" class="relative inline-block text-left">
+              <MenuButton
+                class="inline-flex justify-center rounded-md text-lg hover:text-primary focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+              >
+                Concepts
+              </MenuButton>
+
+              <transition
+                enter-active-class="transition duration-100 ease-out"
+                enter-from-class="transform scale-95 opacity-0"
+                enter-to-class="transform scale-100 opacity-100"
+                leave-active-class="transition duration-75 ease-in"
+                leave-from-class="transform scale-100 opacity-100"
+                leave-to-class="transform scale-95 opacity-0"
+              >
+                <MenuItems
+                  class="absolute right-0 mt-1.5 w-72 origin-top-right rounded bg-white py-2 shadow ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-black"
+                >
+                  <MenuItem v-slot="{ active }">
+                    <ul class="flex flex-col space-y-1.5">
+                      <li
+                        v-for="({ title, href }, idx) in conceptPages"
+                        :key="idx"
+                        class="py-1.5 px-3 hover:bg-pale dark:hover:bg-dark-gray"
+                      >
+                        <a
+                          class="text-sm tracking-tight hover:text-primary md:text-base lg:text-lg"
+                          :class="{ 'bg-blue-500': active }"
+                          :href="href"
+                        >
+                          {{ title }}
+                        </a>
+                      </li>
+                    </ul>
+                  </MenuItem>
+                </MenuItems>
+              </transition>
+            </Menu>
           </li>
           <li class="flex items-center space-x-2 md:space-x-3 lg:space-x-4">
             <ThemeSwitcher client:load />
@@ -47,6 +115,9 @@
 </template>
 
 <script setup lang="ts">
+import { conceptPages, sortedQuickStartPages } from "~/logic/content";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/vue";
+
 const {
   route: { path },
 } = usePage();
