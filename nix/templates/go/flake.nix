8ff2f3f9ef961d38e955a5ebf67d324f53945cba
Luc Perkins <lucperkins@gmail.com>
2022-11-29 13:41:02 -0300

    Provide working Go package build

--- nix/templates/go/flake.nix
+++ nix/templates/go/flake.nix
@@ -4,16 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
     flake-utils.url = "github:numtide/flake-utils";
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, gitignore }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ go_1_19 ];
+        packages.default = pkgs.buildGo119Module {
+          name = "hello-nix-go";
+          src = gitignore.lib.gitignoreSource ./.;
+          vendorSha256 = "sha256-fwJTg/HqDAI12mF1u/BlnG52yaAlaIMzsILDDZuETrI=";
         };
       });
 }
