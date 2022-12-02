ffaa1828b1719cce406f988440907e091756e70e
Luc Perkins <lucperkins@gmail.com>
2022-12-02 11:32:06 -0300

    Add uninstall Nix doc

--- src/components/mdx/admonitions/Callout.vue
+++ src/components/mdx/admonitions/Callout.vue
@@ -1,19 +1,22 @@
 <template>
   <div
-    class="not-prose rounded-lg border-2 border-light-gray bg-pale py-4 px-6 dark:border-gray dark:bg-inherit"
+    class="my-4 rounded-lg border-2 border-light-gray bg-pale py-4 px-6 dark:border-gray dark:bg-inherit"
   >
-    <Disclosure :defaultOpen="open" v-slot="{ open }">
+    <Disclosure as="div" :defaultOpen="open" v-slot="{ open }">
       <DisclosureButton as="div" class="flex items-center justify-between">
-        <span class="text-2xl font-semibold tracking-tight">{{ show }}</span>
+        <span class="text-2xl font-light tracking-tight">{{ show }}</span>
         <IconFaChevronRight
-          class="h-4 w-4 text-primary"
+          class="h-3.5 w-3.5 text-primary"
           :class="{
             'rotate-90 transform duration-200': open,
           }"
           @click="toggle"
         />
       </DisclosureButton>
-      <DisclosurePanel as="div" class="mt-4">
+      <DisclosurePanel
+        as="div"
+        class="not-prose content mt-3.5 text-sm md:text-base lg:text-lg"
+      >
         <slot />
       </DisclosurePanel>
     </Disclosure>
