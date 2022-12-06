a65f25147104cbab57e0d016ab4fb61df1ca029a
Luc Perkins <lucperkins@gmail.com>
2022-12-06 15:22:29 -0300

    Add initial variant of hoverable headings

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,5 @@
+import Heading2 from "~/components/layout/Heading2.vue";
+import Heading3 from "~/components/layout/Heading3.vue";
 import setDarkTheme from "~/logic/dark-mode-set?raw";
 import site from "~/site";
 import { defineApp } from "iles";
@@ -19,5 +21,9 @@ export default defineApp({
       // https://github.com/ElMassimo/iles/blob/cacd1947281008b299793f617757262871ace795/docs/src/logic/dark-color-scheme-check.ts
       { children: setDarkTheme, once: true },
     ],
+  },
+  mdxComponents: {
+    h2: Heading2,
+    h3: Heading3,
   },
 });
---
+++ src/components/layout/Heading3.vue
@@ -0,0 +1,18 @@
+<template>
+  <h3 id="$attrs.id" @mouseover="hover = true" @mouseleave="hover = false">
+    <slot />
+    <span v-if="hover" class="ml-3.5"
+      ><a
+        :href="`#${$attrs.id}`"
+        class="text-gray no-underline hover:text-primary dark:text-light-gray dark:hover:text-primary"
+        >#</a
+      ></span
+    >
+  </h3>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const hover = ref<boolean>(false);
+</script>
