3829d4a5730cb1bdb8bcaba95d8bec4ace06dfab
Luc Perkins <lucperkins@gmail.com>
2022-12-07 15:49:16 -0300

    Fix nix run preview mode

--- flake.nix
+++ flake.nix
@@ -123,7 +123,7 @@
         runLocal = pkgs.writeScriptBin "run-local" ''
           rm -rf dist
           pnpm install
-          pnpm run build
+          ENV=preview pnpm run build
           python3 -m http.server -d dist 3000
         '';
 
