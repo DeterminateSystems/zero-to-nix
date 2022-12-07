7d4c6da1a2c71e40f1adf89089993defbfbc132d
Luc Perkins <lucperkins@gmail.com>
2022-12-07 15:23:16 -0300

    Temporarily remove Nix sources for quick start

--- nix/templates/python/hello.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-def main():
-    print("Hello from inside a Python program built with Nix!")
-
-if __name__ == "__main__":
-    main()
--- nix/templates/rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-extern crate colored;
-
-use colored::*;
-
-fn main() {
-    let nix = "Nix".blue();
-    let rust = "Rust".red();
-    println!("Hello from {} + {}!", nix, rust);
-}
