eed289165cb3b056e55d6aba343b81b21b0d3190
Luc Perkins <lucperkins@gmail.com>
2022-12-15 17:17:07 -0300

    Fix PostHog loading issue

--- src/layouts/base.vue
+++ src/layouts/base.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script setup lang="ts">
-import posthog from "posthog-js";
 import { PageProps } from "iles";
 import site from "~/site";
 
@@ -18,6 +17,10 @@ const canonical = new URL(url, href).toString();
 useHead({
   link: [{ rel: "canonical", href: canonical }],
 });
+</script>
+
+<script client:load lang="ts">
+import { posthog } from "posthog-js";
 
 if (import.meta.env.MODE === "production") {
   const apiKey = "phc_OPJtdGL4gAGdo8VKLsHz4LmKfoOMKkrza1BsBNeUdx4";
