d1469bceb1b214681d050fc74f40971b0389b323
Luc Perkins <lucperkins@gmail.com>
2022-12-20 13:54:09 -0300

    Persist user choice to localStorage

--- src/components/layout/CookieConsent.vue
+++ src/components/layout/CookieConsent.vue
@@ -1,17 +1,29 @@
 <template>
-  <div v-if="noOptionSelected" class="fixed left-0 bottom-0 z-50">
+  <div v-if="!optionSelected" class="fixed left-0 bottom-0 z-50">
     <div
-      class="dark:bg-gray-accent-dark max-w-[202px] translate-x-[15px] translate-y-[-15px] rounded-lg bg-primary text-white/80">
+      class="dark:bg-gray-accent-dark max-w-[202px] translate-x-[15px] translate-y-[-15px] rounded-lg bg-primary text-white/80"
+    >
       <p class="m-0 p-3 text-[14px]">
         We use a single, first-party analytics cookie to focus our limited time
         and energy on the most important documentation. Check out our
-        <a href="https://determinate.systems/privacy" class="underline" target="_blank">privacy policy</a>.
+        <a
+          href="https://determinate.systems/privacy"
+          class="underline"
+          target="_blank"
+          >privacy policy</a
+        >.
       </p>
       <div class="grid grid-cols-2 divide-x border-t border-white/40">
-        <button class="py-2 text-sm font-semibold text-white" @click="acceptCookies">
+        <button
+          class="py-2 text-sm font-semibold text-white"
+          @click="acceptCookies"
+        >
           Accept
         </button>
-        <button class="py-2 text-sm font-semibold text-white" @click="declineCookies">
+        <button
+          class="py-2 text-sm font-semibold text-white"
+          @click="declineCookies"
+        >
           Decline
         </button>
       </div>
@@ -20,18 +32,26 @@
 </template>
 
 <script setup lang="ts">
+import { persistentAtom } from "@nanostores/persistent";
+import { WritableAtom } from "nanostores";
 import { ref } from "vue";
 
-const noOptionSelected = ref<boolean>(true);
+const optionSelectedStorage: WritableAtom<string> = persistentAtom<string>(
+  "zero-to-nix:cookies-option-selected",
+  "false",
+);
+
+const optionSelected = ref<boolean>(optionSelectedStorage.get() === "true");
 
 const acceptCookies = () => {
-  console.log("hello!")
-  noOptionSelected.value = false;
+  console.log("hello!");
+  optionSelected.value = true;
+  optionSelectedStorage.set("true");
 };
 
 const declineCookies = () => {
-  console.log("bye!")
-  noOptionSelected.value = false;
+  console.log("bye!");
+  optionSelected.value = true;
+  optionSelectedStorage.set("true");
 };
-
 </script>
