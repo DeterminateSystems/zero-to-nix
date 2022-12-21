50941fe56e0c0aee19ee9bcff7b1f4d57004de61
Graham Christensen <graham@grahamc.com>
2022-12-21 10:17:41 -0500

    PageSurvey: wip, disable itself if tracking is disabled

--- src/components/layout/PageSurvey.vue
+++ src/components/layout/PageSurvey.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="space-y-4">
-    <PageSurveyButtons v-if="step === 'prompt'" @complete="acceptResponse" />
+    <PageSurveyDisabled
+      v-if="trackingDisabled() && updates >= 0"
+      @complete="updateTracking"
+    />
+    <PageSurveyButtons
+      v-else-if="step === 'prompt'"
+      @complete="acceptResponse"
+    />
     <PageSurveyFeedback
       v-else-if="step === 'feedback'"
       :reaction="reaction"
@@ -11,6 +18,7 @@
 </template>
 
 <script setup lang="ts">
+import { posthog } from "posthog-js";
 import { ref } from "vue";
 
 export type Reaction = "positive" | "negative";
@@ -18,6 +26,17 @@ export type Step = "prompt" | "feedback" | "thankyou";
 
 const reaction = ref<Reaction | undefined>();
 const step = ref<Step>("prompt");
+const updates = ref<number>(1);
+
+const trackingDisabled = () => {
+  return typeof window !== "undefined"
+    ? posthog.has_opted_out_capturing()
+    : false;
+};
+
+const updateTracking = () => {
+  updates.value += 1;
+};
 
 const acceptResponse = (r: Reaction) => {
   reaction.value = r;
---
+++ src/components/layout/PageSurveyDisabled.vue
@@ -0,0 +1,24 @@
+<template>
+  <h3 class="text-lg font-semibold md:text-xl lg:text-2xl">
+    Was this page helpful?
+  </h3>
+  <p>turn on tracking plz</p>
+
+  <button
+    class="flex items-center justify-between space-x-3 rounded-lg border-2 border-light-gray py-2 px-3 text-sm font-semibold tracking-tight hover:border-primary hover:shadow-sm dark:border-gray dark:hover:border-primary md:text-base"
+    @click="enableCookie()"
+  >
+    <span>Opt back in</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import { posthog } from "posthog-js";
+
+const emit = defineEmits(["complete"]);
+
+const enableCookie = () => {
+  posthog.opt_in_capturing();
+  emit("complete");
+};
+</script>
