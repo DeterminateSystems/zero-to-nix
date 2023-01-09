4dc376a675344ebc4547f1cde591c5c0f14c65fe
Luc Perkins <lucperkins@gmail.com>
2023-01-09 11:32:21 -0300

    Add click away close trigger

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -27,12 +27,17 @@
           </a>
 
           <button
-            class="hover:text-primary"
+            class="hover:text-dark-gray dark:hover:text-gray"
             @click="toggleDrawer"
             type="button"
+            id="drawer-toggle"
             data-drawer-target="nav-drawer"
             data-drawer-show="nav-drawer"
             aria-controls="nav-drawer"
+            :class="[
+              showDrawer && 'text-primary',
+              !showDrawer && 'text-gray dark:text-light-gray',
+            ]"
           >
             <IconFaSolidBars class="h-4 w-4" />
           </button>
@@ -69,81 +74,88 @@
     </HorizontalContainer>
   </nav>
 
-  <TransitionRoot
-    :show="showDrawer"
-    enter="transition-opacity duration-100"
-    enter-from="opacity-0"
-    enter-to="opacity-100"
-    leave="transition-opacity duration-150"
-    leave-from="opacity-100"
-    leave-to="opacity-0"
+  <OnClickOutside
+    @trigger="closeDrawer"
+    :options="{ ignore: ['#drawer-toggle'] }"
   >
-    <div
-      id="nav-drawer"
-      tabindex="-1"
-      class="fixed z-40 flex h-full w-5/6 flex-col space-y-2 overflow-y-auto border-t border-r border-light-gray bg-white p-4 dark:border-gray dark:bg-dark"
-      aria-labelledby="nav-drawer"
+    <TransitionRoot
+      :show="showDrawer"
+      enter="transition-opacity duration-100"
+      enter-from="opacity-0"
+      enter-to="opacity-100"
+      leave="transition-opacity duration-150"
+      leave-from="opacity-100"
+      leave-to="opacity-0"
     >
-      <div class="flex justify-end">
-        <button
-          @click="toggleDrawer"
-          data-drawer-hide="nav-drawer"
-          aria-controls="nav-drawer"
-        >
-          <svg
-            aria-hidden="true"
-            class="h-4 w-4 dark:fill-light-gray dark:hover:fill-primary"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      <div
+        id="nav-drawer"
+        tabindex="-1"
+        class="fixed z-40 flex h-full w-5/6 flex-col overflow-y-auto border-t border-r border-light-gray bg-white p-4 dark:border-gray dark:bg-dark"
+        aria-labelledby="nav-drawer"
+        vOnClickOutside="toggleDrawer"
+      >
+        <div class="flex justify-end">
+          <button
+            @click="closeDrawer"
+            data-drawer-hide="nav-drawer"
+            aria-controls="nav-drawer"
           >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-          <span class="sr-only">Close menu</span>
-        </button>
-      </div>
-      <div>
-        <ul class="space-y-1">
-          <li>
-            <a
-              href="/start"
-              class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
-              @click="toggleDrawer"
+            <svg
+              aria-hidden="true"
+              class="h-4 w-4 dark:fill-light-gray dark:hover:fill-primary"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Quick start
-            </a>
-          </li>
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+            <span class="sr-only">Close menu</span>
+          </button>
+        </div>
+        <div>
+          <ul class="space-y-1">
+            <li>
+              <a
+                href="/start"
+                class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
+                @click="closeDrawer"
+              >
+                Quick start
+              </a>
+            </li>
 
-          <li>
-            <a
-              href="/concepts"
-              class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
-              @click="toggleDrawer"
-            >
-              Concepts
-            </a>
-          </li>
+            <li>
+              <a
+                href="/concepts"
+                class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
+                @click="closeDrawer"
+              >
+                Concepts
+              </a>
+            </li>
 
-          <li v-for="({ text, href }, idx) in $site.navbarLinks" :key="idx">
-            <a
-              :href="href"
-              class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
-              @click="toggleDrawer"
-            >
-              {{ text }}
-            </a>
-          </li>
-        </ul>
+            <li v-for="({ text, href }, idx) in $site.navbarLinks" :key="idx">
+              <a
+                :href="href"
+                class="text-lg font-light tracking-tight hover:text-primary md:text-base lg:text-lg"
+                @click="closeDrawer"
+              >
+                {{ text }}
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
-  </TransitionRoot>
+    </TransitionRoot>
+  </OnClickOutside>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { OnClickOutside } from "@vueuse/components";
 import { conceptPages, sortedQuickStartPages } from "~/logic/content";
 import { TransitionRoot } from "@headlessui/vue";
 
@@ -151,5 +163,9 @@ const showDrawer = ref<boolean>(false);
 
 const toggleDrawer = () => {
   showDrawer.value = !showDrawer.value;
+};
+
+const closeDrawer = () => {
+  showDrawer.value = false;
 };
 </script>
