e52a647684449bd63233eeb385b0b480cf290e80
Luc Perkins <lucperkins@gmail.com>
2023-01-11 14:11:00 -0300

    Last set of fixes for admonition padding

--- src/components/layout/Summary.vue
+++ src/components/layout/Summary.vue
@@ -5,9 +5,7 @@
     <div
       class="md:px-4.5 flex flex-col flex-nowrap items-start space-y-2 py-2 px-3 md:flex-row md:space-y-0 md:space-x-6 md:py-3 lg:py-4 lg:px-6"
     >
-      <span class="text-lg font-bold md:text-xl lg:text-2xl">
-        In this guide
-      </span>
+      <span class="font-bold md:text-lg lg:text-xl">In this guide</span>
       <span class="space-y-1.5">
         <div
           v-for="item in items"
