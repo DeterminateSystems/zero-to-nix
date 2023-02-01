fa91cdf382ebc4049605e2facc737f9cfec899af
Luc Perkins <lucperkins@gmail.com>
2023-02-01 15:01:44 -0800

    Add install phase for wrapping executable

--- nix/templates/pkg/scala/flake.nix
+++ nix/templates/pkg/scala/flake.nix
@@ -11,6 +11,9 @@
 
   outputs = { self, nixpkgs, sbt }:
     let
+      pname = "zero-to-nix-scala";
+      version = "0.1.0";
+
       nameValuePair = name: value: { inherit name value; };
       genAttrs = names: f: builtins.listToAttrs (map (n: nameValuePair n (f n)) names);
       allSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
@@ -20,12 +23,27 @@
       });
     in
     {
+      devShells = forAllSystems ({ pkgs, system }: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.sbt ];
+        };
+      });
+
       packages = forAllSystems ({ pkgs, system }: {
         default = sbt.mkSbtDerivation.${system} {
-          pname = "zero-to-nix-scala";
-          version = "0.1.0";
+          inherit pname version;
           depsSha256 = "sha256-ebtaZPbLylPt17/r++1QFWezOBfSUB9J8djkOHOpWbE=";
+          nativeBuildInputs = [ pkgs.makeWrapper ];
           src = ./.;
+          buildPhase = ''
+            sbt package
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp target/scala-2.12/${pname}_2.12-${version}.jar $out/bin/${pname}.jar
+            makeWrapper ${pkgs.jre}/bin/java $out/bin/${pname} \
+              --add-flags "-jar $out/bin/${pname}.jar"
+          '';
         };
       });
     };
