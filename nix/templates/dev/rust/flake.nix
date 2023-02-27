8225eee58ef1a708e54ce230e6212180ea5c2b19
Luc Perkins <lucperkins@gmail.com>
2023-02-27 04:08:54 +0200

    Fix error in Rust env due to missing systems

--- nix/templates/dev/rust/flake.nix
+++ nix/templates/dev/rust/flake.nix
@@ -21,6 +21,14 @@
         })
       ];
 
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
       # Helper to provide system-specific attributes
       forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
         pkgs = import nixpkgs { inherit overlays system; };
