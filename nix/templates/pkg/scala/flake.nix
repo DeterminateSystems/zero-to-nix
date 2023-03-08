a254eaf1eb144117b7b74f9928f1548df69936b4
Luc Perkins <lucperkins@gmail.com>
2023-03-08 15:42:27 +0200

    Fix docs in template

--- nix/templates/pkg/scala/flake.nix
+++ nix/templates/pkg/scala/flake.nix
@@ -1,4 +1,5 @@
-# Indebted to https://github.com/zaninime/sbt-derivation/blob/92d6d6d825e3f6ae5642d1cce8ff571c3368aaf7/templates/cli-app/flake.nix
+# This flake is heavily indebted to the flake template in the sbt-derivation project:
+# https://github.com/zaninime/sbt-derivation/blob/92d6d6d825e3f6ae5642d1cce8ff571c3368aaf7/templates/cli-app/flake.nix
 {
   description = "Scala example flake for Zero to Nix";
 
@@ -16,7 +17,7 @@
     let
       name = "zero-to-nix-scala";
       version = "0.1.0";
-      scalaVersion = "3.2.1";
+      mainClass = "hello";
 
       # Systems supported
       allSystems = [
@@ -35,35 +36,31 @@
     {
       # Package outputs
       packages = forAllSystems ({ pkgs, system }: {
-        default =
-          let
-            mainClass = "hello";
-          in
-          sbt.mkSbtDerivation.${system} {
-            pname = name;
-            inherit version;
-            depsSha256 = "sha256-rFh3dTcK65/sFOy2mQI6HxK+VQdzn3XvBNaVksSvP0U=";
-            src = ./.;
-            depsWarmupCommand = ''
-              sbt 'managedClasspath; compilers'
-            '';
-            startScript = ''
-              #!${pkgs.runtimeShell}
-              exec ${pkgs.openjdk_headless}/bin/java ''${JAVA_OPTS:-} -cp "${
-                placeholder "out"
-              }/share/${name}/lib/*" ${nixpkgs.lib.escapeShellArg mainClass} "$@"
-            '';
-            buildPhase = ''
-              sbt stage
-            '';
-            installPhase = ''
-              libs_dir="$out/share/${name}/lib"
-              mkdir -p "$libs_dir"
-              cp -ar target/universal/stage/lib/. "$libs_dir"
-              install -T -D -m755 $startScriptPath $out/bin/${name}
-            '';
-            passAsFile = [ "startScript" ];
-          };
+        default = sbt.mkSbtDerivation.${system} {
+          pname = name;
+          inherit version;
+          depsSha256 = "sha256-rFh3dTcK65/sFOy2mQI6HxK+VQdzn3XvBNaVksSvP0U=";
+          src = ./.;
+          depsWarmupCommand = ''
+            sbt 'managedClasspath; compilers'
+          '';
+          startScript = ''
+            #!${pkgs.runtimeShell}
+            exec ${pkgs.openjdk_headless}/bin/java ''${JAVA_OPTS:-} -cp "${
+              placeholder "out"
+            }/share/${name}/lib/*" ${nixpkgs.lib.escapeShellArg mainClass} "$@"
+          '';
+          buildPhase = ''
+            sbt stage
+          '';
+          installPhase = ''
+            libs_dir="$out/share/${name}/lib"
+            mkdir -p "$libs_dir"
+            cp -ar target/universal/stage/lib/. "$libs_dir"
+            install -T -D -m755 $startScriptPath $out/bin/${name}
+          '';
+          passAsFile = [ "startScript" ];
+        };
       });
     };
 }
