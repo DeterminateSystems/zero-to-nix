15eddf3d0ac299d6e538c92f9dd027f3fa406296
Luc Perkins <lucperkins@gmail.com>
2023-06-16 12:01:33 -0700

    Update Nixpkgs input

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix-flake-based Node.js development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
   };
 
   outputs =
