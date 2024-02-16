924f31ddc375d06c2d55ea900a4c2d0468b9c12e
Graham Christensen <graham@grahamc.com>
2024-02-16 12:33:41 -0500

    expound on the comment

--- scripts/netlify-redirects.sh
+++ scripts/netlify-redirects.sh
@@ -2,6 +2,9 @@
 
 # Note: www.zero-to-nix.com is in here explicitly and not zero-to-nix.com.
 # Adding zero-to-nix.com would cause an infinite loop back to itself.
+# This does cause www.www.zero-to-nix.com to be in the output, but I think
+# fixing that is more trouble than it is worth ... and makes this script
+# actually complicated.
 
 domains=(
   02nix.com
