8eac777b78d71c175d5b0fe945a240ba16d4e057
Luc Perkins <lucperkins@gmail.com>
2024-06-18 22:09:01 -0700

    Add custom headings

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,7 @@ export default defineConfig({
     }),
     expressiveCode({
       themes: ["github-dark"],
+      emitExternalStylesheet: true,
       defaultProps: {
         showLineNumbers: false,
       },
