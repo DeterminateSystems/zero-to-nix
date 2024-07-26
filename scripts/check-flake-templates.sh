e1ed5fe4ae28f8ca04286f057e6f20847d48919c
Luc Perkins <lucperkins@gmail.com>
2024-07-26 08:48:16 -0700

    Make template checking script fail

--- scripts/check-flake-templates.sh
+++ scripts/check-flake-templates.sh
@@ -1,8 +1,17 @@
+set -euo pipefail
+
+echo "Checking dev shell templates"
+
 for env in ./nix/templates/dev/*; do
+  echo "\_ checking ${env}"
   nix flake check --all-systems $env
 done
 
+echo "Checking package build templates"
+
 for pkg in ./nix/templates/pkg/*; do
+  echo "\_ checking ${pkg}"
   nix flake check --all-systems $pkg
+  echo "\_ building ${pkg}"
   nix build $pkg
 done
