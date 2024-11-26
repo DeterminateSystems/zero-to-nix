29ee926f50b4a87e732d028f3ec171ede247d018
Luc Perkins <lucperkins@gmail.com>
2024-11-25 16:21:04 -0800

    Fix TS error

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,12 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-interface Window {
-  Alpine: import('alpinejs').Alpine;
+import type { PostHog } from "posthog-js";
+
+declare global {
+  interface Window {
+    posthog: PostHog;
+
+    Alpine: import('alpinejs').Alpine;
+  }
 }
