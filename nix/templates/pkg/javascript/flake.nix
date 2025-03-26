116f24a48e5200247df4a76abd09108a5ce70a1a
Graham Christensen <graham@grahamc.com>
2025-03-26 12:14:37 -0400

    Nixpkgs current

--- nix/templates/pkg/javascript/flake.nix
+++ nix/templates/pkg/javascript/flake.nix
@@ -2,7 +2,7 @@
   description = "JavaScript example flake for Zero to Nix";
 
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.2405.*.tar.gz";
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
   };
 
   outputs = { self, nixpkgs }:
