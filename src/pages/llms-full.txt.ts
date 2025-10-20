9fdfa55cb0fbfcf5544cccf8c55c04ba322bf795
Luc Perkins <lucperkins@gmail.com>
2025-10-20 16:25:20 -0300

    Finish first pass at templates

---
+++ src/pages/llms-full.txt.ts
@@ -0,0 +1,62 @@
+import { conceptPagePath, startPagePath } from "../lib/utils";
+import { site } from "../site";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import Handlebars from "handlebars";
+import fs from "node:fs";
+import path from "node:path";
+
+const { url: root, title, description: tagline, llms } = site;
+
+const templateFile = fs.readFileSync(
+  path.join(process.cwd(), "src/templates/llms-full.txt.hbs"),
+  "utf-8",
+);
+
+const template = Handlebars.compile(templateFile);
+
+export const GET: APIRoute = async () => {
+  const startPages = await getCollection("start");
+  const conceptPages = await getCollection("concepts");
+
+  const content = template({
+    root,
+    title,
+    system:
+      "This is the complete documentation for Zero to Nix, including the learning journey (start) and concept pages.",
+    instructions:
+      "Learning journey (start) and concept page items begin and end with a --- and all page titles are in bold.",
+    tagline,
+    description: llms.description,
+    startPages: startPages.map(({ data: { title }, slug, body }) => ({
+      title,
+      href: `${root}/${startPagePath(slug)}`,
+      content: body,
+    })),
+    conceptPages: conceptPages.map(({ data: { title }, slug, body }) => ({
+      title,
+      href: `${root}/${conceptPagePath(slug)}`,
+      content: body,
+    })),
+    otherFormats: [
+      {
+        root,
+        file: "llms.txt",
+        description: "the main version",
+      },
+      {
+        root,
+        file: "llms-full.txt",
+        description: "complete content in one file",
+      },
+    ],
+  });
+
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+};
+
+export const prerender = true;
---
+++ src/pages/llms-small.txt.ts
@@ -0,0 +1,53 @@
+//import { conceptPagePath, startPagePath } from "../lib/utils";
+import { site } from "../site";
+import type { APIRoute } from "astro";
+import { getCollection } from "astro:content";
+import Handlebars from "handlebars";
+import fs from "node:fs";
+import path from "node:path";
+
+const { url: root, title, llms } = site;
+
+const templateFile = fs.readFileSync(
+  path.join(process.cwd(), "src/templates/llms-small.txt.hbs"),
+  "utf-8",
+);
+
+const template = Handlebars.compile(templateFile);
+
+export const GET: APIRoute = async () => {
+  const startPages = await getCollection("start");
+  const conceptPages = await getCollection("concepts");
+
+  const content = template({
+    root,
+    title,
+    description: llms.description,
+    startPages: startPages.map(({ data: { title } }) => ({
+      title,
+    })),
+    conceptPages: conceptPages.map(({ data: { title } }) => ({
+      title,
+    })),
+    otherFormats: [
+      {
+        root,
+        file: "llms.txt",
+        description: "the main version",
+      },
+      {
+        root,
+        file: "llms-full.txt",
+        description: "complete content in one file",
+      },
+    ],
+  });
+
+  return new Response(content, {
+    headers: {
+      "Content-Type": "text/plain; charset=utf-8",
+    },
+  });
+};
+
+export const prerender = true;
