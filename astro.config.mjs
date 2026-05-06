4cdaad566e540572f5703062d82a2b9417d1a2ee
Luc Perkins <lucperkins@gmail.com>
2026-05-06 17:23:54 -0500

    Use env vars more smartly

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,43 +3,48 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 import expressiveCode from "astro-expressive-code";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
 import rehypeExternalLinks from "rehype-external-links";
 import remarkEmoji from "remark-emoji";
 import remarkHeadingId from "remark-heading-id";
 
+const port = 3000;
+const site = process.env.ENV === "production"
+  ? "https://zero-to-nix.com"
+  : process.env.DEPLOY_PRIME_URL ?? `http://localhost:${port}`;
+
 export default defineConfig({
-  integrations: [
-    alpinejs({
-      entrypoint: "./src/entrypoint",
-    }),
-    expressiveCode(),
-    icon(),
-    mdx({
-      //gfm: true,
-      remarkPlugins: [remarkEmoji, remarkHeadingId],
-    }),
-    sitemap(),
-    tailwind(),
-    react(),
-  ],
-  markdown: {
-    rehypePlugins: [
-      [
-        rehypeExternalLinks,
-        { rel: ["nofollow noopener noreferrer"], target: "_blank" },
-      ],
-    ],
-  },
-  prefetch: {
-    prefetchAll: true,
-    defaultStrategy: "hover",
-  },
-  server: {
-    open: true,
-    port: 3000,
-  },
-  site: "https://zero-to-nix.com",
+	integrations: [
+		alpinejs({
+			entrypoint: "./src/entrypoint",
+		}),
+		expressiveCode(),
+		icon(),
+		mdx({
+			//gfm: true,
+			remarkPlugins: [remarkEmoji, remarkHeadingId],
+		}),
+		sitemap(),
+		tailwind(),
+		react(),
+	],
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{ rel: ["nofollow noopener noreferrer"], target: "_blank" },
+			],
+		],
+	},
+	prefetch: {
+		prefetchAll: true,
+		defaultStrategy: "hover",
+	},
+	server: {
+		open: true,
+		port,
+	},
+	site,
 });
